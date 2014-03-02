5e663253bee11bdb42251c549108b7b17a4ffa84
mahendra <mahendra@mahendra.(none)>
2014-03-02 14:59:56 +0530

    on timeout do not increment the term-id and go to follower state.

---
+++ RaftMain/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	Raft "github.com/mgkanani/raft"
+//	"sync"
+)
+
+var total_servers = 7
+
+func main() {
+	flagid := flag.Int("pid", 1, "flag type is integer")
+	flag.Parse()
+        myid := *flagid
+	ch := make(chan int)
+	//valid := InitServer(myid, "./config.json",true , ch)
+	_ = Raft.InitServer(myid, "./config.json",true , ch)
+
+//	wg := new(sync.WaitGroup)
+//	wg.Add(1)
+//	wg.Wait()
+	return
+
+}
