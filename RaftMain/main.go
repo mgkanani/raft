6248032dcc07050e4f8f62a3bce150936d517c92
mahendra <mahendra@mahendra.(none)>
2014-03-08 12:46:44 +0530

    disabling the debug mode

--- RaftMain/main.go
+++ RaftMain/main.go
@@ -16,7 +16,7 @@ func main() {
 	wg := new(sync.WaitGroup)
 	//ch := make(chan int)
 	//valid := InitServer(myid, "./config.json",true , ch)
-	valid, rafttype := Raft.InitServer(myid, "./config.json", true)
+	valid, rafttype := Raft.InitServer(myid, "./config.json", false)
 
 	if valid {
 		wg.Add(1)
@@ -30,7 +30,7 @@ func main() {
 
 func printData(rafttype *Raft.RaftType,myid int) {
 	for {
-		time.Sleep(5 * time.Second)
+		time.Sleep(3100 * time.Millisecond)
 		println("[",myid,"]","Current Term is :-",rafttype.CurTerm(), "Current Leader is:-",rafttype.Leader())
 	}
 }
