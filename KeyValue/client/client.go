b0b8cc2ef23544da0dd806ed7e5d9db70161a1d7
mahendra <mahendra@mahendra-System-Product-Name.(none)>
2014-04-02 17:04:03 +0530

    KeyValue_server.go is the same file which was previously refered as KeyValue.go, Testing of SET,GET,UPDATE,DELETE are performed manually. It is working Perfectly.

--- KeyValue/client/client.go
+++ KeyValue/client/client.go
@@ -9,43 +9,42 @@ import (
 )
 
 const (
-        SET    = 1
-        GET    = 2
-        UPDATE = 3
-        DELETE = 4
+	SET    = 1
+	GET    = 2
+	UPDATE = 3
+	DELETE = 4
 )
 
-
 type Message struct {
-	Type int //SET,GET,UPDATE,DELETE
-	Key string
-	Value  string
+	Type  int //SET,GET,UPDATE,DELETE
+	Key   string
+	Value string
 }
 
 func main() {
-	msg := &Message{Type:GET, Key:"abc",Value:"testing"}
+	msg := &Message{Type: GET, Key: "abc", Value: "testing"}
 	buf, _ := json.Marshal(msg)
 	body := bytes.NewBuffer(buf)
 	fmt.Println(body)
 	//Send(body,"http://127.0.0.1:8081/")
-	Send(msg,"http://127.0.0.1:45003/")
+	Send(msg, "http://127.0.0.1:45003/")
 }
 
-func Send(msg *Message,url string){
+func Send(msg *Message, url string) {
 	buf, _ := json.Marshal(msg)
 	body := bytes.NewBuffer(buf)
-        r, e := http.Post(url, "text/json", body)
-	if e == nil{
+	r, e := http.Post(url, "text/json", body)
+	if e == nil {
 		fmt.Println(r)
-	        if r.Request.Method == "GET"{
-        	        fmt.Println("Redirected to:-",r.Request.URL.String(),e,body)
-			Send(msg,r.Request.URL.String())
-	        }else{
-	        	fmt.Println("Not redirected:-",e,r.Request.Method,r.Request.URL,body)
-        	        response, _ := ioutil.ReadAll(r.Body)
-                	fmt.Println(string(response),r.Header,"if err:-",e)
+		if r.Request.Method == "GET" {
+			fmt.Println("Redirected to:-", r.Request.URL.String(), e, body)
+			Send(msg, r.Request.URL.String())
+		} else {
+			fmt.Println("Not redirected:-", e, r.Request.Method, r.Request.URL, body)
+			response, _ := ioutil.ReadAll(r.Body)
+			fmt.Println(string(response), r.Header, "if err:-", e)
 		}
-	}else{
-		fmt.Println("error:-",e)
+	} else {
+		fmt.Println("error:-", e)
 	}
 }
